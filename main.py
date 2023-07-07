1f47ee8c00d88ff7fdb803471626da0a2bbcec51
shanshee <shansheehan@gmail.com>
2023-07-06 22:54:16 -0700

    PING PONG GAME PROJECT

---
+++ main.py
@@ -0,0 +1,55 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Ping Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+screen.onkey(l_paddle.go_up, "W")
+screen.onkey(l_paddle.go_down, "S")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # detect if right paddle misses
+    if ball.xcor() > 400:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # detect if right paddle misses
+    if ball.xcor() < -400:
+        ball.reset_position()
+        scoreboard.r_point()
+
+    game_is_on = scoreboard.game_over()
+
+
+
+screen.exitonclick()
